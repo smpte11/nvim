5fc93e7aae922803d7c9e2fac632d60fb7458b7f
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2025-03-12 10:25:23 +0100

    Create mini picker for chezmoi

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -1,21 +1,21 @@
 pick_chezmoi = function()
-    local pick = MiniPick
+	local pick = MiniPick
 
-    -- Define the source items
-    local source_items = require("chezmoi.commands").list()
+	-- Define the source items
+	local source_items = require("chezmoi.commands").list()
 
-    -- Start the picker
-    pick.start({
-        source = {
-            items = source_items,
-            choose = function(selected)
-                require("chezmoi.commands").edit({
-                    targets = { "~/" .. selected[1] },
-                    args = { "--watch" }
-                })
-            end,
-        },
-    })
+	-- Start the picker
+	pick.start({
+		source = {
+			items = source_items,
+			choose = function(selected)
+				require("chezmoi.commands").edit({
+					targets = { "~/" .. selected },
+					args = { "--watch" },
+				})
+			end,
+		},
+	})
 end
 
-vim.api.nvim_command('command! ChezmoiPick lua pick_chezmoi()')
+vim.api.nvim_command("command! ChezmoiPick lua pick_chezmoi()")
