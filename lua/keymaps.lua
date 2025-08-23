847e7c47c3fc9482b1ae29f88541a3ea8c04213b
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2025-08-23 22:26:39 +0200

    Fix unescaped character

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -262,5 +262,5 @@ keymap("n", "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "Navigate left" 
 keymap("n", "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", { desc = "Navigate down" })
 keymap("n", "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", { desc = "Navigate up" })
 keymap("n", "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", { desc = "Navigate right" })
-keymap("n", "<c-\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", { desc = "Navigate previous" })
+keymap("n", "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", { desc = "Navigate previous" })
 -- stylua: ignore end
